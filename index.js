6b144159575242bc5604a442d106c41e96311009
bit_knox <johanflensmark@gmail.com>
2019-01-21 14:32:14 +0000

    Framework for recursive encoding was added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
-buildTree('test ')
 
+var originalTree = buildTree('twojastarazapierdala')
+treeToCodes(buildTree('twojastarazapierdala'), "")
+//console.log(JSON.stringify(buildTree('twojastarazapierdala')))
+//console.log(originalTree.left)
 function Node (data, right, left) {
   this.data = data
   this.right = right
@@ -8,15 +11,15 @@ function Node (data, right, left) {
 }
 
 function frequency (string) {
-  var frekvens = {}
+  var frequency = {}
   for (let value of string) {
-    if (frekvens[value] === undefined) {
-      frekvens[value] = 1
+    if (frequency[value] === undefined) {
+      frequency[value] = 1
     } else {
-      frekvens[value] += 1
+      frequency[value] += 1
     }
   }
-  return frekvens
+  return frequency
 }
 
 function sortfreq (freqs) {
@@ -31,7 +34,7 @@ function buildTree (string) {
   Array.prototype.remove = function (index) {
     this.splice(index, 1)
   }
-   var freqss = frequency(string)
+  var freqss = frequency(string)
   var sorted = sortfreq(freqss)
   while (sorted.length > 1) {
     var tree = new Node(sorted[0][0] + sorted[1][0], sorted[1][1], sorted[0][1])
@@ -40,5 +43,32 @@ function buildTree (string) {
     sorted.remove(0)
     sorted.push([index, tree])
   }
-  console.log(JSON.stringify(sorted))
+  return sorted[0]
+}
+var prefix = ""
+var ary = []
+function treeToCodes(tree, prefix){
+if(tree !== null){
+    for(var k in tree){
+      if(typeof(tree[k]) == "object"){
+        if(isNaN(tree[k])){
+          if(tree[k].right == "object"){
+            treeToCodes(tree[k].right, prefix +"1")
+          }
+          else {
+            console.log("Found leaf" + prefix)
+          }
+          if(tree[k].left == "object"){
+            treeToCodes(tree[k].left, prefix+"0")
+          } else {
+            console.log("Found leaf" + prefix)
+          }
+      } else{
+        console.log("Found end...")
+      }
+  }
+}
+} else {
+  console.log("Tree was null")
+}
 }
