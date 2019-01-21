9a7e668eef5b929e57b79b812f653e84ebab4ea9
bit_knox <johanflensmark@gmail.com>
2019-01-21 08:55:12 +0000

    Style fixed to fit JavaScript standard

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 
-buildTree("test ")
+buildTree('test ')
 
-function Node(data, right, left){
-  this.data = data;
-  this.right = right;
-  this.left = left;
+function Node (data, right, left) {
+  this.data = data
+  this.right = right
+  this.left = left
 }
 
-function frequency(string){
+function frequency (string) {
   var frekvens = {}
   for (let value of string) {
-    if(frekvens[value] == undefined){
-        frekvens[value] = 1;
-  } else {
-    frekvens[value] += 1
+    if (frekvens[value] === undefined) {
+      frekvens[value] = 1
+    } else {
+      frekvens[value] += 1
+    }
   }
-}
-return frekvens
-}
-
-
-function sortfreq(freqs){
-
-var letterfreq=[];
-for( var let in freqs){
-  letterfreq.push([freqs[let],let]);
-}
-return letterfreq.sort();
+  return frekvens
 }
 
-function buildTree(string){
-Array.prototype.remove = function(index) {
-      this.splice(index, 1);
+function sortfreq (freqs) {
+  var letterfreq = []
+  for (var letters in freqs) {
+    letterfreq.push([freqs[letters], letters])
+  }
+  return letterfreq.sort()
 }
-  freqss = frequency(string)
-  sorted = sortfreq(freqss)
-    while(sorted.length>1){
-      var tree = new Node(sorted[0][0]+sorted[1][0],sorted[1][1],sorted[0][1])
-      var index = sorted[0][0]+sorted[1][0]
-      sorted.remove(0)
-      sorted.remove(0)
-      sorted.push([index, tree])
-}
-console.log(JSON.stringify(sorted))
+
+function buildTree (string) {
+  Array.prototype.remove = function (index) {
+    this.splice(index, 1)
+  }
+   var freqss = frequency(string)
+  var sorted = sortfreq(freqss)
+  while (sorted.length > 1) {
+    var tree = new Node(sorted[0][0] + sorted[1][0], sorted[1][1], sorted[0][1])
+    var index = sorted[0][0] + sorted[1][0]
+    sorted.remove(0)
+    sorted.remove(0)
+    sorted.push([index, tree])
+  }
+  console.log(JSON.stringify(sorted))
 }
