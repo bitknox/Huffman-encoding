a7bdc60ce37c2a52e947d00af36ad33e14feda78
bitknox <johanflensmark@gmail.com>
2019-01-21 09:42:55 +0100

    Cleaned up index

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+buildTree("test ")
+
 function Node(data, right, left){
   this.data = data;
   this.right = right;
@@ -8,8 +10,8 @@ function Node(data, right, left){
 function frequency(string){
   var frekvens = {}
   for (let value of string) {
-  if(frekvens[value] == undefined){
-    frekvens[value] = 1;
+    if(frekvens[value] == undefined){
+        frekvens[value] = 1;
   } else {
     frekvens[value] += 1
   }
@@ -17,25 +19,28 @@ function frequency(string){
 return frekvens
 }
 
-Array.prototype.remove = function(index) {
-    this.splice(index, 1);
-}
+
 function sortfreq(freqs){
 
 var letterfreq=[];
 for( var let in freqs){
-letterfreq.push([freqs[let],let]);
+  letterfreq.push([freqs[let],let]);
 }
 return letterfreq.sort();
 }
-freqss = frequency("abcdegcaa")
-sorted = sortfreq(freqss)
-while(sorted.length>1){
-var tree = new Node(sorted[0][0]+sorted[1][0],sorted[1][1],sorted[0][1])
-var index = sorted[0][0]+sorted[1][0]
-sorted.remove(0)
-sorted.remove(0)
-sorted.push([index, tree])
-}
 
+function buildTree(string){
+Array.prototype.remove = function(index) {
+      this.splice(index, 1);
+}
+  freqss = frequency(string)
+  sorted = sortfreq(freqss)
+    while(sorted.length>1){
+      var tree = new Node(sorted[0][0]+sorted[1][0],sorted[1][1],sorted[0][1])
+      var index = sorted[0][0]+sorted[1][0]
+      sorted.remove(0)
+      sorted.remove(0)
+      sorted.push([index, tree])
+}
 console.log(JSON.stringify(sorted))
+}
